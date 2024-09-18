e1fa17c86eeca7559484165b4166a223ffe8a240
rasulomaroff <rasulomaroff@gmail.com>
2024-09-17 19:59:04 -0700

    config: inits vite config

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+
+export default {
+    /* Postcss plugins should come first, if added */
+    plugins: [autoprefixer, tailwindcss]
+}
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import React from '@vitejs/plugin-react'
+import TSConfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [React(), TSConfigPaths()]
+})
