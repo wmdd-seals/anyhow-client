84abc8542b0eb6a8282092cf9394239af1e63783
tom555-555 <tomok.development@gmail.com>
2024-10-08 16:57:01 -0700

    update(button component): changed a prop 'buttonType' to optinal item

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -2,7 +2,7 @@ import { Button as HeadlessButton } from '@headlessui/react'
 import type { ComponentPropsWithRef, PropsWithChildren, ReactNode } from 'react'
 
 type ButtonProps = {
-    buttonType: 'default' | 'dark' | 'light'
+    buttonType?: 'default' | 'dark' | 'light'
 } & ComponentPropsWithRef<'button'>
 
 export function Button(props: PropsWithChildren<ButtonProps>): ReactNode {
