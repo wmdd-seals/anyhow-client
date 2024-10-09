6a32dd3753fea4027b87ac3b8c331fdf131436c4
tom555-555 <tomok.development@gmail.com>
2024-10-08 17:04:56 -0700

    chore: add button component to ui/header branch

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,10 +1,32 @@
 import { Button as HeadlessButton } from '@headlessui/react'
 import type { ComponentPropsWithRef, PropsWithChildren, ReactNode } from 'react'
 
-type ButtonProps = {} & ComponentPropsWithRef<'button'>
+type ButtonProps = {
+    buttonType?: 'default' | 'dark' | 'light'
+} & ComponentPropsWithRef<'button'>
 
 export function Button(props: PropsWithChildren<ButtonProps>): ReactNode {
-    const { children, ...rest } = props
+    const { children, buttonType = 'default', ...rest } = props
 
-    return <HeadlessButton {...rest}>{children}</HeadlessButton>
+    console.log(buttonType)
+
+    const buttonStyles = {
+        default: {
+            backgroundColor: 'bg-slate-800',
+            textColor: 'text-white'
+        },
+        dark: { backgroundColor: 'bg-slate-800', textColor: 'text-white' },
+        light: { backgroundColor: 'bg-white', textColor: 'text-black' }
+    }
+
+    const { backgroundColor, textColor } = buttonStyles[buttonType]
+
+    return (
+        <HeadlessButton
+            className={`h-12 px-5 py-3 rounded-md justify-center items-center gap-2 inline-flex font-bold ${backgroundColor} ${textColor}`}
+            {...rest}
+        >
+            {children}
+        </HeadlessButton>
+    )
 }
