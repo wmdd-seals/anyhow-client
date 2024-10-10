6b96627953902c95a312d91b0a155794792670da
tom555-555 <tomok.development@gmail.com>
2024-10-10 15:31:12 -0700

    conflict(shared/ui/index): resolved

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,7 +1,3 @@
 export * from './text-input'
 export * from './button'
-<<<<<<< HEAD
 export * from './dropdown'
-=======
-export * from './tag'
->>>>>>> ccefa32 (chore(tags): make it exposed with barrel pattern)
