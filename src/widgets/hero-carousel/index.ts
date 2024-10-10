652ae1e6ff5e4401a6aa6bad3752c4ccae0254a0
tom555-555 <tomok.development@gmail.com>
2024-10-10 06:10:06 -0700

    faet(carousel):- Adjusted styles to support mobile, tablet, and desktop views

--- src/widgets/hero-carousel/index.ts
+++ src/widgets/hero-carousel/index.ts
@@ -1,1 +1,1 @@
-export { default as HeroCarousel } from './ui/hero-carousel'
+export { HeroCarousel } from './ui/hero-carousel'
--- src/widgets/hero-carousel/ui/hero-carousel.tsx
+++ src/widgets/hero-carousel/ui/hero-carousel.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
-import { useMediaQuery, mediaQuery } from 'src/shared/lib/mediaQuery'
 
 const responsive = {
     desktop: {
@@ -56,112 +55,60 @@ const carouselItems = [
     }
 ]
 
-const PCCarousel = (): React.ReactNode => (
-    <Carousel
-        additionalTransfrom={0}
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        keyBoardControl
-        minimumTouchDrag={80}
-        pauseOnHover
-        renderButtonGroupOutside={false}
-        renderDotsOutside
-        responsive={responsive}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-        arrows={false}
-    >
-        {carouselItems.map((item, index) => (
-            <div key={index} className="relative aspect-[2.84/1]">
-                <a href="#" className="block absolute inset-0">
-                    <img
-                        src={item.imageUrl}
-                        className="w-full h-full object-cover"
-                        alt={item.title}
-                    />
-                </a>
-                <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end pl-8 pb-8 w-2/3">
-                    <h2 className="text-black font-bold mb-2 text-[56px]">
-                        {item.title}
-                    </h2>
-                    <p className="text-[#717D96] text-[22px]">
-                        {item.description}
-                    </p>
-                </div>
-            </div>
-        ))}
-    </Carousel>
-)
-
-const MobileCarousel = (): React.ReactNode => (
-    <Carousel
-        additionalTransfrom={0}
-        autoPlaySpeed={3000}
-        centerMode={false}
-        className=""
-        containerClass="container"
-        dotListClass=""
-        draggable
-        focusOnSelect={false}
-        infinite
-        keyBoardControl
-        minimumTouchDrag={80}
-        pauseOnHover
-        renderButtonGroupOutside={false}
-        renderDotsOutside
-        responsive={responsive}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        shouldResetAutoplay
-        showDots
-        sliderClass=""
-        slidesToSlide={1}
-        swipeable
-        arrows={false}
-    >
-        {carouselItems.map((item, index) => (
-            <div key={index} className="relative aspect-[1/1.54]">
-                <a href="#" className="block absolute inset-0">
-                    <img
-                        src={item.imageUrl}
-                        className="w-full h-full object-cover"
-                        alt={item.title}
-                    />
-                </a>
-                <div className="absolute inset-x-0 bottom-1/4 flex flex-col justify-center p-4">
-                    <h2 className="text-black font-bold mb-2 text-5xl">
-                        {item.title}
-                    </h2>
-                    <p className="text-[#717D96] text-base">
-                        {item.description}
-                    </p>
-                </div>
-            </div>
-        ))}
-    </Carousel>
-)
-
-const HeroCarousel: React.FC = () => {
-    const isMobile = useMediaQuery(mediaQuery.sp)
-
+function HeroCarousel(): React.ReactNode {
     return (
-        <div className="pb-8 relative">
-            {isMobile ? <MobileCarousel /> : <PCCarousel />}
+        <div className="relative flex justify-center">
+            <Carousel
+                additionalTransfrom={0}
+                autoPlaySpeed={3000}
+                centerMode={false}
+                className=""
+                containerClass="container"
+                draggable
+                focusOnSelect={false}
+                infinite
+                keyBoardControl
+                minimumTouchDrag={80}
+                pauseOnHover
+                dotListClass="absolute -bottom-8"
+                renderButtonGroupOutside={false}
+                renderDotsOutside={true}
+                responsive={responsive}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+                arrows={false}
+            >
+                {carouselItems.map((item, index) => (
+                    <div
+                        key={index}
+                        className="relative aspect-[1/1.54] md:aspect-[2.84/1] mx-auto"
+                    >
+                        <a href="#" className="block absolute inset-0">
+                            <img
+                                src={item.imageUrl}
+                                className="w-full h-full object-cover object-center"
+                                alt={item.title}
+                            />
+                        </a>
+                        <div className="absolute inset-x-0 bottom-0 md:bottom-1/4 flex flex-col justify-end pl-8 pb-8 md:p-4 w-2/3">
+                            <h2 className="text-black font-bold mb-2 text-[56px] md:text-5xl">
+                                {item.title}
+                            </h2>
+                            <p className="text-[#717D96] text-[22px] md:text-base">
+                                {item.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </Carousel>
         </div>
     )
 }
 
-export default HeroCarousel
+export { HeroCarousel }
