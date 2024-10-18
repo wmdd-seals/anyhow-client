7d9f9f2c9618651eb0d2c03695267aba5ab71585
tom555-555 <tomok.development@gmail.com>
2024-10-17 21:59:36 -0700

    feat(homepage): gql code gen

--- src/_gqlgen/gql.ts
+++ src/_gqlgen/gql.ts
@@ -17,6 +17,7 @@ const documents = {
     "\n    query Guide($id: ID!) {\n        res: guide(id: $id) {\n            id\n            title\n            body\n            tags\n            user {\n                id\n                firstName\n                lastName\n            }\n        }\n    }\n": types.GuideDocument,
     "\n    query SignIn($input: UserSignInInput) {\n        signIn(input: $input) {\n            message\n            token\n        }\n    }\n": types.SignInDocument,
     "\n    mutation CreateGuide($input: GuideCreationInput!) {\n        res: createGuide(input: $input) {\n            id\n            title\n            tags\n            description\n            body\n        }\n    }\n": types.CreateGuideDocument,
+    "\n    query Guides {\n        res: guides {\n            description\n            title\n            id\n            tags\n        }\n    }\n": types.GuidesDocument,
 };
 
 /**
@@ -45,6 +46,10 @@ export function graphql(source: "\n    query SignIn($input: UserSignInInput) {\n
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(source: "\n    mutation CreateGuide($input: GuideCreationInput!) {\n        res: createGuide(input: $input) {\n            id\n            title\n            tags\n            description\n            body\n        }\n    }\n"): (typeof documents)["\n    mutation CreateGuide($input: GuideCreationInput!) {\n        res: createGuide(input: $input) {\n            id\n            title\n            tags\n            description\n            body\n        }\n    }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n    query Guides {\n        res: guides {\n            description\n            title\n            id\n            tags\n        }\n    }\n"): (typeof documents)["\n    query Guides {\n        res: guides {\n            description\n            title\n            id\n            tags\n        }\n    }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
