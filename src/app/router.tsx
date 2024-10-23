1d2320a6121e95bdb84432377ec9fad7f13c82f9
tom555-555 <tomok.development@gmail.com>
2024-10-22 23:45:07 -0700

    feat(router): wrap the protected routes with auth router provider

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -14,6 +14,7 @@ import {
     EditGuidePage,
     Dashboard
 } from '../pages'
+import ProtectedRoute from './protectedroute'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -21,17 +22,18 @@ const router = createBrowserRouter(
             <Route path="/">
                 <Route path="login" element={<LoginPage />} />
                 <Route index element={<HomePage />} />
-                <Route path="/:id">
-                    <Route index element={<GuidePage />} />
-
-                    <Route path="edit">
-                        <Route index element={<EditGuidePage />} />
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/:id">
+                        <Route index element={<GuidePage />} />
+                        <Route path="edit">
+                            <Route index element={<EditGuidePage />} />
+                        </Route>
+                        <Route path="quiz/:id" element={<QuizPage />} />
+                        <Route path="account" element={<AccountPage />} />
+                        <Route path="create" element={<CreateGuidePage />} />
+                        <Route path="dashboard" element={<Dashboard />} />
                     </Route>
-
-                    <Route path="quiz/:id" element={<QuizPage />} />
                 </Route>
-                <Route path="account" element={<AccountPage />} />
-                <Route path="dashboard" element={<Dashboard />} />
             </Route>
         </>
     )
