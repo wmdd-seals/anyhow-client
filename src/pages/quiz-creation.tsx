40e2cd67a3ba1b520cccba14bba04e997a2bd6e4
Summer-Children <nogata00@mylangara.ca>
2024-10-24 21:38:44 -0700

    fix:move the logic of getting the guideId to page component

--- src/pages/quiz-creation.tsx
+++ src/pages/quiz-creation.tsx
@@ -2,12 +2,18 @@ import type { ReactNode } from 'react'
 import { Header } from '../widgets/header/ui/header'
 import { Footer } from '../widgets/footer/ui/footer'
 import { QuizCreation } from '../widgets/quiz-creation'
+import { useParams } from 'react-router-dom'
 
 export function QuizCreationPage(): ReactNode {
+    const { id:guideId } = useParams<{ id: string }>()
+
     return (
         <div className="min-h-screen flex flex-col gap-3">
             <Header />
-            <QuizCreation />
+
+            {!guideId && 'Sorry, quiz not found'}
+            
+            {guideId && <QuizCreation className='grow' guideId={guideId}/>}
             <Footer />
         </div>
     )
