55231062ba67a894e408f5d8961b24fc373ef5f2
Summer-Children <nogata00@mylangara.ca>
2024-10-28 10:36:07 -0700

    feat:costom hook of use-save-answer

--- src/widgets/quiz-challenge/api/save-quiz-answer.ts
+++ src/widgets/quiz-challenge/api/save-quiz-answer.ts
@@ -1,6 +1,6 @@
 import { graphql } from '@gqlgen'
 
-const SAVE_QUIZ_ANSWER = graphql(`
+export const SAVE_QUIZ_ANSWER = graphql(`
     mutation SaveQuizAnswer($input: SaveQuizAnswersInput) {
         res: saveQuizAnswers(input: $input) {
             id
