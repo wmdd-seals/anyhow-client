6be572445e287b918c3e348453f1ed057459e923
Summer-Children <nogata00@mylangara.ca>
2024-10-30 03:27:49 -0700

    chore:delete the unncessary file

--- src/pages/quiz-challenge.tsx
+++
@@ -1,6 +0,0 @@
-import { type ReactNode } from 'react'
-import { QuizChallenge } from '../widgets/quiz-challenge'
-
-export function QuizChallengePage(): ReactNode {
-    return <QuizChallenge></QuizChallenge>
-}
