a89d0ba393941a4bd968796b94a2150e02b2e769
Tomoki Kaneko <61049646+tom555-555@users.noreply.github.com>
2024-10-31 01:33:58 -0700

    Fix: changes for authenticated routes (#30)
    * fix(auth-route): apply correct placement on each
    
    * fix(header): add z-index

--- src/_gqlgen/graphql.ts
+++ src/_gqlgen/graphql.ts
@@ -16,6 +16,15 @@ export type Scalars = {
   Float: { input: number; output: number; }
   /** Custom scalar for JSON Object and Array */
   JSON: { input: any; output: any; }
+  /** Base64 to Stream Conversion */
+  Stream: { input: any; output: any; }
+};
+
+export type FileInfo = {
+  base64Data: Scalars['String']['input'];
+  guideId: Scalars['String']['input'];
+  mimeType: Scalars['String']['input'];
+  name: Scalars['String']['input'];
 };
 
 export type GenerateQuizInput = {
@@ -60,6 +69,7 @@ export type QuizInput = {
 
 export type SaveQuizAnswersInput = {
   answers: Scalars['JSON']['input'];
+  iscompleted?: InputMaybe<Scalars['Boolean']['input']>;
   quizid: Scalars['ID']['input'];
 };
 
@@ -111,14 +121,14 @@ export type CreateGuideMutationVariables = Exact<{
 }>;
 
 
-export type CreateGuideMutation = { __typename?: 'Mutation', res: { __typename?: 'Guide', id: string, title?: string | null, tags: any, description?: string | null, body?: string | null } };
+export type CreateGuideMutation = { __typename?: 'Mutation', res: { __typename?: 'Guide', id?: string | null, title?: string | null, tags?: any | null, description?: string | null, body?: string | null } };
 
 export type GuideQueryVariables = Exact<{
   id: Scalars['ID']['input'];
 }>;
 
 
-export type GuideQuery = { __typename?: 'Query', res: { __typename?: 'Guide', id: string, title?: string | null, body?: string | null, tags: any, user: { __typename?: 'User', id: string, firstName: string, lastName: string } } };
+export type GuideQuery = { __typename?: 'Query', res: { __typename?: 'Guide', id?: string | null, title?: string | null, body?: string | null, tags?: any | null, user?: { __typename?: 'User', id: string, firstName: string, lastName: string } | null } };
 
 export type SignInQueryVariables = Exact<{
   input?: InputMaybe<UserSignInInput>;
@@ -132,19 +142,19 @@ export type GetGuideQueryVariables = Exact<{
 }>;
 
 
-export type GetGuideQuery = { __typename?: 'Query', res: { __typename?: 'Guide', id: string, title?: string | null, body?: string | null, tags: any } };
+export type GetGuideQuery = { __typename?: 'Query', res: { __typename?: 'Guide', id?: string | null, title?: string | null, body?: string | null, tags?: any | null } };
 
 export type UpdateGuideMutationVariables = Exact<{
   input: UpdateGuideInput;
 }>;
 
 
-export type UpdateGuideMutation = { __typename?: 'Mutation', res: { __typename?: 'Guide', id: string } };
+export type UpdateGuideMutation = { __typename?: 'Mutation', res: { __typename?: 'Guide', id?: string | null } };
 
 export type GuidesQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type GuidesQuery = { __typename?: 'Query', res: Array<{ __typename?: 'Guide', description?: string | null, title?: string | null, id: string, tags: any } | null> };
+export type GuidesQuery = { __typename?: 'Query', res: Array<{ __typename?: 'Guide', description?: string | null, title?: string | null, id?: string | null, tags?: any | null } | null> };
 
 export type SaveFavoriteTopicsMutationVariables = Exact<{
   input?: InputMaybe<UserProfile>;
