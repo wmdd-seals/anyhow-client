41438f681e29cf201bff647370224f1d25178171
rasulomaroff <rasulomaroff@gmail.com>
2024-10-30 21:25:07 -0700

    feat(edit-guide): implements upload/remove guide cover features

--- src/widgets/edit-guide/ui/edit-guide.tsx
+++ src/widgets/edit-guide/ui/edit-guide.tsx
@@ -1,5 +1,5 @@
 import { Button, TextEditor, TextInput } from '@shared/ui'
-import { type ReactElement, type ReactNode } from 'react'
+import { useCallback, useRef, type ReactElement, type ReactNode } from 'react'
 import { Controller, FormProvider } from 'react-hook-form'
 import { useForm } from 'react-hook-form'
 import cn from 'clsx'
@@ -7,6 +7,9 @@ import { GET_GUIDE_QUERY } from '../api/get-guide'
 import { useMutation, useQuery } from '@apollo/client'
 import { UPDATE_GUIDE_MUTATION } from '../api/update-guide'
 import { useNavigate } from 'react-router-dom'
+import { UPLOAD_GUIDE_COVER } from '../api/upload-guide-cover'
+import { toBase64 } from '@shared/lib/file'
+import { REMOVE_GUIDE_COVER } from '../api/remove-guide-cover'
 
 type UseCreateGuideForm = {
     title: string
@@ -32,7 +35,13 @@ export function EditGuide(props: EditGuideProps): ReactNode {
 
     const navigate = useNavigate()
 
-    const [updateGuide] = useMutation(UPDATE_GUIDE_MUTATION)
+    const [updateGuideMutation] = useMutation(UPDATE_GUIDE_MUTATION)
+    const [uploadCoverMutation] = useMutation(UPLOAD_GUIDE_COVER)
+    const [removeCoverMutation] = useMutation(REMOVE_GUIDE_COVER, {
+        variables: { id }
+    })
+
+    const coverImageRef = useRef<HTMLImageElement>(null)
 
     const form = useForm<UseCreateGuideForm>({
         defaultValues,
@@ -49,12 +58,63 @@ export function EditGuide(props: EditGuideProps): ReactNode {
     const progress =
         ((body.replaceAll('\n', '').length * 0.95) / (1500 * 6)) * 100
 
+    const selectCoverAndUpload = useCallback(() => {
+        const input = document.createElement('input')
+        input.type = 'file'
+        input.accept = 'image/*'
+        input.multiple = false
+
+        input.addEventListener('change', async () => {
+            const cover = input.files?.[0]
+            if (!cover) return
+
+            await uploadCoverMutation({
+                variables: {
+                    input: {
+                        name: cover.name,
+                        guideId: id,
+                        mimeType: cover.type,
+                        base64Data: await toBase64(cover)
+                    }
+                }
+            })
+
+            coverImageRef.current!.src = getGuideCoverUrl(id)
+        })
+
+        input.click()
+    }, [])
+
+    const removeCover = useCallback(() => {
+        void removeCoverMutation()
+
+        coverImageRef.current!.src = `/guide-cover-thumbnail.jpg`
+    }, [])
+
     if (loading) return 'Loading...'
 
     if (!data?.res) return 'Something went wrong...'
 
     return (
         <FormProvider {...form}>
+            <div className="relative">
+                <img
+                    ref={coverImageRef}
+                    src={getGuideCoverUrl(id)}
+                    onError={(e): void => {
+                        // if not loaded for any reason, use thumbnail
+                        e.currentTarget.src = `/guide-cover-thumbnail.jpg`
+                    }}
+                    alt="Guide Cover Thumbnail"
+                    className="max-w-[1300px] mx-auto"
+                />
+
+                <div className="absolute left-1/2 -translate-x-1/2 bottom-14 flex items-center gap-2">
+                    <Button onClick={selectCoverAndUpload}>Edit</Button>
+                    <Button onClick={removeCover}>Delete</Button>
+                </div>
+            </div>
+
             <div
                 className={cn(
                     'flex flex-col max-w-[50rem] mx-auto w-full',
@@ -156,7 +216,7 @@ export function EditGuide(props: EditGuideProps): ReactNode {
 
                 <Button
                     onClick={form.handleSubmit(async data => {
-                        const guide = await updateGuide({
+                        const guide = await updateGuideMutation({
                             variables: {
                                 input: {
                                     id,
@@ -177,5 +237,13 @@ export function EditGuide(props: EditGuideProps): ReactNode {
                 </Button>
             </div>
         </FormProvider>
+    )
+}
+
+function getGuideCoverUrl(id: string): string {
+    return (
+        import.meta.env.VITE_API_URL +
+        import.meta.env.VITE_IMAGES_ENDPOINT +
+        `/${id}`
     )
 }
