0774c63a345a23016245baa4366de82c8c618b50
Summer-Children <nogata00@mylangara.ca>
2024-10-31 19:30:41 -0700

    feat: move the logic to fetch a whole quiz to QuizChallenge component

--- src/widgets/quiz-challenge/ui/quiz-challenge.tsx
+++ src/widgets/quiz-challenge/ui/quiz-challenge.tsx
@@ -1,19 +1,37 @@
 import { useState, useEffect, type ReactNode } from 'react'
 import { useQuery } from '@apollo/client'
-import type { QuestionInput } from '@gqlgen/graphql'
 import { QuizScoreModal } from './quiz-score-modal'
 import { Button } from '@shared/ui'
-import { GET_QUIZ_ANSWERS } from '@widgets/quiz-challenge'
-import { useSaveQuizAnswer } from '@widgets/quiz-challenge'
+import { GET_QUIZ_ANSWERS } from '@widgets/quiz-challenge/api/get-quiz-answers'
+import { useSaveQuizAnswer } from '@widgets/quiz-challenge/api/use-save-answers'
+import { GET_QUIZ_QUERY } from '../../../entities/quiz'
 
 interface QuizChallengeProp {
+    guideId: string
     quizId: string
-    quizLoading: boolean
-    quizData: QuestionInput[]
 }
 
 export function QuizChallenge(props: QuizChallengeProp): ReactNode {
-    const { quizId, quizLoading, quizData } = props
+    const { guideId, quizId } = props
+
+    const { data: quizInfo, loading: quizLoading } = useQuery(GET_QUIZ_QUERY, {
+        variables: {
+            guideId: guideId
+        },
+        skip: !guideId
+    })
+
+    const quizData =
+        quizInfo?.res?.quiz?.body?.quiz?.questions
+            ?.filter(question => question?.options)
+            .map(question => ({
+                questionTitle: question!.questionTitle,
+                options: question!.options.filter(
+                    (option): option is string => option !== null
+                ),
+                correctAnswerIndex: question!.correctAnswerIndex
+            })) || []
+
     const [correctness, setCorrectness] = useState<string[]>([])
     const [selectedOptions, setSelectedOptions] = useState<number[]>(
         Array(quizData.length).fill(-1)
@@ -64,69 +82,78 @@ export function QuizChallenge(props: QuizChallengeProp): ReactNode {
 
     const formattedQuizData = quizData.map(question => ({
         questionTitle: question.questionTitle,
-        options: question.options.filter(Boolean) as string[],
+        options: question.options.filter(Boolean),
         correctAnswerIndex: question.correctAnswerIndex
     }))
 
     return (
-        <div className="p-4 flex flex-col gap-3">
-            <h1 className="text-3xl font-bold">Test your learning</h1>
-            <ul className="flex flex-col gap-5">
-                {quizLoading && <h1 className="text-lg">Loading...</h1>}
+        <>
+            {quizId && (
+                <div className="p-4 flex flex-col gap-3">
+                    <h1 className="text-3xl font-bold">Test your learning</h1>
+                    <ul className="flex flex-col gap-5">
+                        {quizLoading && <h1 className="text-lg">Loading...</h1>}
 
-                {!quizLoading &&
-                    formattedQuizData.map((question, questionIndex) => (
-                        <li key={questionIndex}>
-                            <h2 className="text-xl font-bold">
-                                Question {questionIndex + 1}:{' '}
-                                {question.questionTitle}
-                            </h2>
+                        {!quizLoading &&
+                            formattedQuizData.map((question, questionIndex) => (
+                                <li key={questionIndex}>
+                                    <h2 className="text-xl font-bold">
+                                        Question {questionIndex + 1}:{' '}
+                                        {question.questionTitle}
+                                    </h2>
 
-                            <ul>
-                                {question.options.map(
-                                    (option, selectedOptionIndex) => (
-                                        <li key={selectedOptionIndex}>
-                                            <label className="flex items-center gap-2">
-                                                <input
-                                                    type="radio"
-                                                    name={`question-${questionIndex}`}
-                                                    value={selectedOptionIndex}
-                                                    onChange={() =>
-                                                        handleOptionChange(
-                                                            questionIndex,
-                                                            selectedOptionIndex
-                                                        )
-                                                    }
-                                                    checked={
-                                                        selectedOptions[
-                                                            questionIndex
-                                                        ] ===
-                                                        selectedOptionIndex
-                                                    }
-                                                    className="form-radio text-slate-800"
-                                                />
-                                                {option}
-                                            </label>
-                                        </li>
-                                    )
-                                )}
-                            </ul>
-                        </li>
-                    ))}
-            </ul>
+                                    <ul>
+                                        {question.options.map(
+                                            (option, selectedOptionIndex) => (
+                                                <li key={selectedOptionIndex}>
+                                                    <label className="flex items-center gap-2">
+                                                        <input
+                                                            type="radio"
+                                                            name={`question-${questionIndex}`}
+                                                            value={
+                                                                selectedOptionIndex
+                                                            }
+                                                            onChange={() =>
+                                                                handleOptionChange(
+                                                                    questionIndex,
+                                                                    selectedOptionIndex
+                                                                )
+                                                            }
+                                                            checked={
+                                                                selectedOptions[
+                                                                    questionIndex
+                                                                ] ===
+                                                                selectedOptionIndex
+                                                            }
+                                                            className="form-radio text-slate-800"
+                                                        />
+                                                        {option}
+                                                    </label>
+                                                </li>
+                                            )
+                                        )}
+                                    </ul>
+                                </li>
+                            ))}
+                    </ul>
 
-            <Button onClick={checkCorrectness} disabled={!allQuestionsAnswered}>
-                Complete
-            </Button>
+                    <Button
+                        onClick={checkCorrectness}
+                        disabled={!allQuestionsAnswered}
+                    >
+                        Complete
+                    </Button>
 
-            {showModal && (
-                <QuizScoreModal
-                    Correctness={correctness}
-                    questions={formattedQuizData}
-                    setShowModal={setShowModal}
-                    selectedOptions={selectedOptions}
-                />
+                    {showModal && (
+                        <QuizScoreModal
+                            Correctness={correctness}
+                            questions={formattedQuizData}
+                            setShowModal={setShowModal}
+                            selectedOptions={selectedOptions}
+                        />
+                    )}
+                </div>
             )}
-        </div>
+        </>
     )
 }
