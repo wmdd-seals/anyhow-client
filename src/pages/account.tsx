103126f63bc10ee5dda6d9cda041b5fea76189f1
tom555-555 <tomok.development@gmail.com>
2024-11-08 01:31:12 -0800

    feat(accountPage): add UI

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,5 +1,32 @@
+import { useAuth } from '@shared/lib'
+import { TextInput } from '@shared/ui'
+import { Button } from '@shared/ui'
+import { Header } from '@widgets/header/ui/header'
 import type { ReactNode } from 'react'
 
 export function AccountPage(): ReactNode {
-    return <div>Account page</div>
+    const { user } = useAuth()
+    return (
+        <>
+            <Header />
+            <section className="container mx-auto px-7 sm:px-0 box-border flex flex-col gap-4 max-w-[650px]">
+                <h1 className="text-3xl font-bold">Account Setting</h1>
+                <div className="flex flex-col gap-6 bg-slate-100 p-8 rounded-lg">
+                    <div className="flex gap-4 items-center">
+                        <div className="rounded-full bg-white w-20 h-20 flex items-center justify-center text-2xl font-bold">
+                            {user?.firstName[0].toUpperCase()}
+                            {user?.lastName[0].toUpperCase()}
+                        </div>
+                        <Button className="w-fit h-10">Edit</Button>
+                    </div>
+                    <TextInput label="First Name" />
+                    <TextInput label="Last Name" />
+                    <TextInput label="Bio" />
+                    <div className="flex justify-end ">
+                        <Button>Save Changes</Button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
 }
