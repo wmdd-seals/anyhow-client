752c8ec3c8071754dae0bf1145ac8c8781e9b510
tom555-555 <tomok.development@gmail.com>
2024-11-07 21:25:25 -0800

    update(charts): styling

--- src/widgets/statistics/time-spent-bar-chart/ui/index.tsx
+++ src/widgets/statistics/time-spent-bar-chart/ui/index.tsx
@@ -78,35 +78,35 @@ function TimeSpentBarChart(): ReactNode {
                 </div>
             ) : (
                 <>
-            <button
-                onClick={handlePrev}
-                className="text-[#2D3648] absolute left-[10px] top-1/2"
-            >
-                <ArrowLeft size={20} />
-            </button>
-            <button
-                onClick={handleNext}
-                className="text-[#2D3648] absolute right-[10px] top-1/2"
-            >
-                <ArrowRight size={20} />
-            </button>
-            <div className="text-2xl w-full text-left absolute left-5 top-5 font-bold">
-                <label>Completed Guides</label>
+                    <button
+                        onClick={handlePrev}
+                        className="text-[#2D3648] absolute left-[10px] top-1/2"
+                    >
+                        <ArrowLeft size={20} />
+                    </button>
+                    <button
+                        onClick={handleNext}
+                        className="text-[#2D3648] absolute right-[10px] top-1/2"
+                    >
+                        <ArrowRight size={20} />
+                    </button>
+                    <div className="text-2xl w-full text-left absolute left-5 top-5 font-bold">
+                        <label>Completed Guides</label>
                         <p className="text-base">{`${new Date(from).toLocaleDateString()} - ${new Date(to).toLocaleDateString()}`}</p>
-            </div>
+                    </div>
                     <div className="w-11/12 mx-auto max-w-[350px] md:max-w-[450px] h-full">
-            <BarChart
-                data={counts.res.map(item => ({
-                    date: item.date
-                        ? item.date.split('-')[1] +
-                          '/' +
-                          item.date.split('-')[2]
-                        : '',
-                    count: item.count || 0
-                }))}
-                keys={['count']}
-                indexBy="date"
-            />
+                        <BarChart
+                            data={counts.res.map(item => ({
+                                date: item.date
+                                    ? item.date.split('-')[1] +
+                                      '/' +
+                                      item.date.split('-')[2]
+                                    : '',
+                                count: item.count || 0
+                            }))}
+                            keys={['count']}
+                            indexBy="date"
+                        />
                     </div>
                 </>
             )}
