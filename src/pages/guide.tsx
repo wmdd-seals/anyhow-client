cda06da12999f040e27c0288f53413806594b4ba
Summer-Children <nogata00@mylangara.ca>
2024-11-08 03:38:38 -0800

    chore:fix the type error

--- src/pages/guide.tsx
+++ src/pages/guide.tsx
@@ -12,7 +12,7 @@ import { GuideChat } from '@widgets/guide-chat'
 import { Transition, TransitionChild } from '@headlessui/react'
 import { getGuideProgress, Tag } from 'src/entities/guide'
 import { STORE_GUIDE_COMPLETED } from 'src/features/store-guide-completed/api/store-guide-completed'
-import { ArrowRight, Zap } from 'react-feather'
+import { Zap, ArrowRight } from 'react-feather'
 import { GET_GUIDE_COMPLETED_LIST } from '@widgets/quiz-challenge/api/get-guide-completed-list'
 
 const GUIDE_QUERY = graphql(`
