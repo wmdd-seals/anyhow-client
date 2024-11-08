1f3e5f70f94b8e20f5c4cc6003b9bdeba0ebcca9
Tomoki Kaneko <61049646+tom555-555@users.noreply.github.com>
2024-11-07 21:36:26 -0800

    Styling for the chart and data format (#44)
    * update(dashboardCharts): styles, data formatting
    
    * update(charts): styling on charts
    
    * update(charts): styling

--- src/widgets/statistics/time-spent-bar-chart/ui/index.tsx
+++ src/widgets/statistics/time-spent-bar-chart/ui/index.tsx
@@ -4,6 +4,8 @@ import { ResponsiveBar } from '@nivo/bar'
 
 import { useQuery } from '@apollo/client'
 import { graphql } from '@gqlgen'
+import { ArrowLeft, ArrowRight } from 'react-feather'
+
 type BarChartProps = {
     data: {
         date: string
@@ -25,14 +27,16 @@ const GUIDE_COMPLETED_COUNTS = graphql(`
 function BarChart({ data, keys, indexBy }: BarChartProps): ReactNode {
     return (
         <ResponsiveBar
-            colors={['grey']}
+            colors={['#32D430']}
             keys={keys}
             data={data}
             indexBy={indexBy}
+            borderRadius={5}
+            enableLabel={false}
             enableGridX={false}
             enableGridY={false}
             axisLeft={null}
-            margin={{ top: 90, right: 10, bottom: 30, left: 30 }}
+            margin={{ top: 90, right: 10, bottom: 30, left: 10 }}
         />
     )
 }
@@ -43,29 +47,16 @@ function TimeSpentBarChart(): ReactNode {
     const [from, setFrom] = useState<number>(
         new Date(today.setDate(today.getDate() - 7)).getTime()
     )
-    const { data: counts } = useQuery(GUIDE_COMPLETED_COUNTS, {
+    console.log({ today })
+    const { data: counts, loading } = useQuery(GUIDE_COMPLETED_COUNTS, {
         variables: {
             input: {
-                start: new Date(from)
-                    .toISOString()
-                    .split('T')[0]
-                    .replaceAll('-', '/'),
-                end: new Date(to)
-                    .toISOString()
-                    .split('T')[0]
-                    .replaceAll('-', '/')
+                start: new Date(from).toISOString().split('T')[0],
+                end: new Date(to).toISOString().split('T')[0]
             }
         },
         fetchPolicy: 'network-only'
     })
-
-    if (!counts?.res.length) return null
-
-    const averageTimeSpent =
-        Math.floor(
-            counts.res.reduce((acc, curr) => acc + (curr.count || 0), 0) /
-                counts.res.length
-        ) * 30
 
     const handlePrev = (): void => {
         const [newFrom, newTo] = adjustDateRange(from, to, -7)
@@ -80,32 +71,45 @@ function TimeSpentBarChart(): ReactNode {
     }
 
     return (
-        <div className="relative flex flex-col items-center gap-2 border-solid border-2 border-gray-300 rounded-md p-4 h-96">
-            <button
-                onClick={handlePrev}
-                className="bg-blue-500 text-white absolute left-0 top-1/3"
-            >
-                ←
-            </button>
-            <button
-                onClick={handleNext}
-                className="bg-blue-500 text-white absolute right-0 top-1/3"
-            >
-                →
-            </button>
-            <div className="text-2xl text-gray-500 w-full text-left absolute left-5 top-5">
-                <label>Average</label>
-                <p>{averageTimeSpent} minutes</p>
-                <p></p>
-            </div>
-            <BarChart
-                data={counts.res.map(item => ({
-                    date: item.date || '',
-                    count: item.count || 0
-                }))}
-                keys={['count']}
-                indexBy="date"
-            />
+        <div className="relative flex flex-col items-center gap-2 border-solid rounded-md p-4 h-96 shadow-md shadow-lg">
+            {loading || !counts?.res.length ? (
+                <div className="w-full h-full flex items-center justify-center">
+                    <p>Loading</p>
+                </div>
+            ) : (
+                <>
+                    <button
+                        onClick={handlePrev}
+                        className="text-[#2D3648] absolute left-[10px] top-1/2"
+                    >
+                        <ArrowLeft size={20} />
+                    </button>
+                    <button
+                        onClick={handleNext}
+                        className="text-[#2D3648] absolute right-[10px] top-1/2"
+                    >
+                        <ArrowRight size={20} />
+                    </button>
+                    <div className="text-2xl w-full text-left absolute left-5 top-5 font-bold">
+                        <label>Completed Guides</label>
+                        <p className="text-base">{`${new Date(from).toLocaleDateString()} - ${new Date(to).toLocaleDateString()}`}</p>
+                    </div>
+                    <div className="w-11/12 mx-auto max-w-[350px] md:max-w-[450px] h-full">
+                        <BarChart
+                            data={counts.res.map(item => ({
+                                date: item.date
+                                    ? item.date.split('-')[1] +
+                                      '/' +
+                                      item.date.split('-')[2]
+                                    : '',
+                                count: item.count || 0
+                            }))}
+                            keys={['count']}
+                            indexBy="date"
+                        />
+                    </div>
+                </>
+            )}
         </div>
     )
 }
