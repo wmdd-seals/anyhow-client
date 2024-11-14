ada068bd114d8a2f6c51990ce919481e5edeef09
Summer-Children <nogata00@mylangara.ca>
2024-11-13 18:24:51 -0800

    chore: move the query to the feature layer

--- src/widgets/edit-guide/api/remove-guide-cover.ts
+++
@@ -1,7 +0,0 @@
-import { graphql } from '@gqlgen'
-
-export const REMOVE_GUIDE_COVER = graphql(`
-    mutation RemoveGuideCover($id: String!) {
-        res: removeImage(id: $id)
-    }
-`)
--- src/widgets/edit-guide/api/upload-guide-cover.ts
+++
@@ -1,9 +0,0 @@
-import { graphql } from '@gqlgen'
-
-export const UPLOAD_GUIDE_COVER = graphql(`
-    mutation UploadGuideCover($input: FileInfo!) {
-        res: uploadCoverImage(input: $input) {
-            id
-        }
-    }
-`)
