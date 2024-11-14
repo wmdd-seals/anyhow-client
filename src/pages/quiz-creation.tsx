9aaaa984b12e332c9e3e4f2f9936e4889fda2f60
Summer-Children <nogata00@mylangara.ca>
2024-11-13 18:25:17 -0800

    feat:add coverImage component to quiz-creation page

--- src/pages/quiz-creation.tsx
+++ src/pages/quiz-creation.tsx
@@ -3,6 +3,7 @@ import { Header } from '../widgets/header/ui/header'
 import { Footer } from '../widgets/footer/ui/footer'
 import { QuizCreation } from '../widgets/quiz-creation'
 import { useParams } from 'react-router-dom'
+import { CoverImage } from 'src/features/upload-cover-image'
 
 export function QuizCreationPage(): ReactNode {
     const { id: guideId } = useParams<{ id: string }>()
@@ -10,9 +11,8 @@ export function QuizCreationPage(): ReactNode {
     return (
         <div className="min-h-screen flex flex-col gap-3">
             <Header />
-
             {!guideId && 'Sorry, quiz not found'}
-
+            {guideId && <CoverImage id={guideId} />}
             {guideId && <QuizCreation className="grow" guideId={guideId} />}
             <Footer />
         </div>
