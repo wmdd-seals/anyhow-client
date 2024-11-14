46c52f63f996bb142ead88f33b1c3e31de91e619
tom555-555 <tomok.development@gmail.com>
2024-11-14 15:13:45 -0800

    feat(dashboard): add bookmarked guide list on learner dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -14,6 +14,7 @@ import { CardStrip, getGuideProgress } from 'src/entities/guide'
 import { getTimeSpentOnCreatingGuide } from 'src/entities/guide/lib/get-time-spent-on-creating-guide'
 import { useAuth } from '@shared/lib'
 import { Loading } from '@widgets/loading'
+import { PanelGuideList } from '@widgets/guide-list'
 
 const GUIDE_COMPLETED_COUNTS = graphql(`
     query GuideCompletedCounts($input: GuideCompletedDateRange) {
@@ -122,9 +123,9 @@ const Dashboard = (): ReactNode => {
 
     if (loading || loadingGuidesCreated) return <Loading />
     return (
-        <div className="flex flex-col gap-4 ">
+        <div className="flex flex-col gap-10 ">
             <Header />
-            <main className="container mx-auto flex flex-col gap-10 px-10">
+            <main className="container mx-auto flex flex-col gap-10 px-10 gap-y-16">
                 <section className="flex flex-col gap-4 p-8 rounded-3xl bg-gradient-to-r from-blue-950 from-60% to-90% to-green-800">
                     <section className="flex flex-col gap-4">
                         <h2 className="text-5xl leading-tight font-bold mb-5 text-white">
@@ -245,6 +246,14 @@ const Dashboard = (): ReactNode => {
                         </div>
                     )}
                 </section>
+                {!isCreator && (
+                    <section className="flex flex-col gap-4">
+                        <h3 className="text-3xl font-bold">
+                            Bookmarked guides
+                        </h3>
+                        <PanelGuideList filter="bookmarked" />
+                    </section>
+                )}
             </main>
             <Footer />
         </div>
